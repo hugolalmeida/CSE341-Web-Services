aadb17682f4e75abf673798ee2828357aa468d56
hugolalmeida <hugogule@gmail.com>
2023-09-22 19:05:43 -0300

    try 4

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongodb = require('./DB/connection.js');
+const mongodb = require('./db/connection.js');
 
 
 const port = process.env.PORT || 3000;
