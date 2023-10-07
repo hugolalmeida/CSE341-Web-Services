520cae879aedf2cc654369a41a6c9e2f0bd9cee8
hugolalmeida <hugogule@gmail.com>
2023-10-07 11:31:03 -0300

    allow all to change

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,29 +1,33 @@
 const mongodb = require('../db/connection');
 const ObjectId = require('mongodb').ObjectId;
 
-const getAll = async (req, res, next) => {
-  
+const getAll = async (req, res) => {
+  try{
   const result = await mongodb.getDb().db().collection('contacts').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
   });
-
+} catch(err){
+  res.status(500).json({message: err.message});
+}
 };
 
-const getSingle = async (req, res, next) => {
-  
+const getSingle = async (req, res) => {
+  try{
   const userId = new ObjectId(req.params.id);
   const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists[0]);
   });
-
+} catch(err){
+  res.status(500).json(err);
+}
 };
 
 const createContact = async (req, res) => {
-  
+  try{
   const contact = {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
@@ -37,10 +41,13 @@ const createContact = async (req, res) => {
   } else {
     res.status(500).json(response.error || 'Some error occurred while creating the contact.');
   }
-
+} catch(err){
+  res.status(500).json(err);
+}
 };
 
 const updateContact = async (req, res) => {
+  try{
   const userId = new ObjectId(req.params.id);
   // be aware of updateOne if you only want to update specific fields
   const contact = {
@@ -61,11 +68,13 @@ const updateContact = async (req, res) => {
   } else {
     res.status(500).json(response.error || 'Some error occurred while updating the contact.');
   }
-
+} catch(err){
+  res.status(500).json(err);
+}
 };
 
 const deleteContact = async (req, res) => {
-  
+  try{
   const userId = new ObjectId(req.params.id);
   const response = await mongodb
     .getDb()
@@ -78,7 +87,9 @@ const deleteContact = async (req, res) => {
   } else {
     res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
   }
-
+} catch(err){
+  res.status(500).json(err);
+}
 };
 
 module.exports = {
