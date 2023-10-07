1c59689b7fa79fdf3a4493409057589094ca9fca
hugolalmeida <hugogule@gmail.com>
2023-10-07 11:28:21 -0300

    try 5

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -41,7 +41,6 @@ const createContact = async (req, res) => {
 };
 
 const updateContact = async (req, res) => {
-  try{
   const userId = new ObjectId(req.params.id);
   // be aware of updateOne if you only want to update specific fields
   const contact = {
@@ -62,13 +61,11 @@ const updateContact = async (req, res) => {
   } else {
     res.status(500).json(response.error || 'Some error occurred while updating the contact.');
   }
-} catch(err){
-  res.status(500).json(err);
-}
+
 };
 
 const deleteContact = async (req, res) => {
-  try{
+  
   const userId = new ObjectId(req.params.id);
   const response = await mongodb
     .getDb()
@@ -81,9 +78,7 @@ const deleteContact = async (req, res) => {
   } else {
     res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
   }
-} catch(err){
-  res.status(500).json(err);
-}
+
 };
 
 module.exports = {
